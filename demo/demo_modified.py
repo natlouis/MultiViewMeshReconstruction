c8448b08f4a3633070280c17eb39bffe96b5f90c
Nathan Louis <natlouis@umich.edu>
2020-04-22 23:10:42 -0400

    render view for any model, aligned to R2N2 images

--- demo/demo_modified.py
+++ demo/demo_modified.py
@@ -9,7 +9,7 @@ import os
 import torch
 from detectron2.utils.logger import setup_logger
 from pytorch3d.io import save_obj
-from pytorch3d.structures import Meshes
+from pytorch3d.structures import Meshes, Textures
 
 from fvcore.common.file_io import PathManager
 
@@ -20,7 +20,23 @@ import shapenet.utils.vis as vis_utils
 from shapenet.data.utils import image_to_numpy, imagenet_deprocess 
 from shapenet.config import get_shapenet_cfg
 
+from shapenet.utils.coords import project_verts 
+
+from pytorch3d.renderer import (
+    look_at_view_transform,
+    OpenGLPerspectiveCameras,
+    PointLights,
+    DirectionalLights,
+    Materials,
+    RasterizationSettings,
+    MeshRenderer,
+    MeshRasterizer,
+    SoftSilhouetteShader,
+    HardPhongShader
+)
+
 import cv2
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger("demo")
 
@@ -42,10 +58,65 @@ class VisualizationDemo(object):
 
       def run_on_image(self, image, id_str):
         deprocess = imagenet_deprocess(rescale_image=False)
-        voxel_scores, meshes_pred = self.predictor(image)
+
+        with torch.no_grad():
+            voxel_scores, meshes_pred = self.predictor(image)
+
+        sid,mid,iid = id_str.split('-')
+        iid = int(iid)
+
+        #Transform vertex space
+        metadata_path = os.path.join('./datasets/shapenet/ShapeNetV1processed', sid, mid, "metadata.pt")
+        metadata = torch.load(metadata_path)
+        K = metadata["intrinsic"]
+        RTs = metadata["extrinsics"]
+        rot_y_90 = torch.tensor([[0, 0, 1, 0], 
+                                    [0, 1, 0, 0], 
+                                    [-1, 0, 0, 0], 
+                                    [0, 0, 0, 1]]).to(RTs) 
+
+        mesh = meshes_pred[-1][0]
+        invRT = torch.inverse(RTs[iid].mm(rot_y_90))
+        mesh._verts_list[0] = project_verts(mesh._verts_list[0], invRT.cpu())
+
+        #Get look at view extrinsics
+        render_metadata_path = os.path.join('datasets/shapenet/ShapeNetRenderingExtrinsics', sid, mid, 'rendering_metadata.pt')
+        render_metadata = torch.load(render_metadata_path)
+        render_RTs = render_metadata['extrinsics']
+
+        plt.figure(figsize=(10, 10))
+        #R, T = look_at_view_transform(dist=0.742719815206*1.75, elev=27.0590432267, azim=-19.5372820907)
+        R = render_RTs[iid][:3,:3].unsqueeze(0)
+        T = render_RTs[iid][:3,3].unsqueeze(0)
+
+        cameras = OpenGLPerspectiveCameras(R=R, T=T)
+        lights = PointLights(location=[[0.0, 0.0, -3.0]])
+        raster_settings = RasterizationSettings(
+            image_size=512, 
+            blur_radius=0.0, 
+            faces_per_pixel=1, 
+            bin_size=0
+        )   
+        renderer = MeshRenderer(
+            rasterizer=MeshRasterizer(
+            cameras=cameras, 
+            raster_settings=raster_settings
+            ),  
+        shader=HardPhongShader(lights=lights)
+        )   
+
+        verts, faces = mesh.get_mesh_verts_faces(0)
+        verts_rgb = torch.ones_like(verts)[None]
+        textures = Textures(verts_rgb=verts_rgb)
+        mesh.textures = textures 
+
+        rendered_image = renderer(meshes_world=mesh, R=R, T=T)
+
+        plt.imshow(rendered_image[0, ..., :3].cpu().numpy())
+        plt.show()
 
         img = image_to_numpy(deprocess(image[0]))
-        vis_utils.visualize_prediction(id_str, img, meshes_pred[-1][0], self.output_dir)
+        vis_utils.visualize_prediction(id_str, img, mesh, self.output_dir)
 
 def setup_cfg(args):
     cfg = get_shapenet_cfg()
@@ -86,7 +157,7 @@ if __name__ == "__main__":
 
 #     data_dir = './datasets/shapenet/ShapeNetV1processed'
     data_dir = args.data_dir
-    dataset = MeshVoxDataset(data_dir)
+    dataset = MeshVoxDataset(data_dir, return_mesh=False)
 
     #Randomly select an image from the ShapeNet dataset if index args is empty
     if args.index is None:
@@ -96,7 +167,10 @@ if __name__ == "__main__":
 
     item = dataset[idx] #(img, verts, faces, points, normals, voxels, P, id_str)
     img = item[0].unsqueeze(0)
-    id_str = item[-1] 
+    verts = item[1]
+    faces = item[2]
+    P     = item[6]
+    id_str = item[7] 
 
     prediction = demo.run_on_image(img, id_str)
     logger.info("Predictions saved in %s" % (args.output))
